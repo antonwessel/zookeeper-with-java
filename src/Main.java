ff39aee4107c8ebb0d25c7f6812e00c9329993f9
antonwessel <anton.wessel01@outlook.com>
2026-03-24 03:11:41 +0100

    add camel ascii

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 void main() {
-    System.out.println("I love animals!");
-    System.out.println("Let's check on the animals...");
-    System.out.println("The deer looks fine.");
-    System.out.println("The bat looks happy.");
-    System.out.println("The lion looks healthy.");
+    System.out.println("""
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""");
 }
